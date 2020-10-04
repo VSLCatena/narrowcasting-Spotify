320b6f919dfe135d7b5447e60bcd1538451a413a
Kipjr <martijn9873@Hotmail.com>
2020-10-04 12:03:38 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Spotify Accounts Authentication Examples
 
-This project contains basic demos showing the different OAuth 2.0 flows for [authenticating against the Spotify Web API](https://developer.spotify.com/web-api/authorization-guide/).
+This project contains basic demo showing the different OAuth 2.0 flows for [authenticating against the Spotify Web API](https://developer.spotify.com/web-api/authorization-guide/).
 
-These examples cover:
+This example cover:
 
 * Authorization Code flow
-* Client Credentials flow
-* Implicit Grant flow
 
 ## Installation
 
@@ -24,7 +22,7 @@ To do so, go to [your Spotify for Developers Dashboard](https://beta.developer.s
 * http://localhost:8888 (needed for the implicit grant flow)
 * http://localhost:8888/callback
 
-Once you have created your app, replace the `client_id`, `redirect_uri` and `client_secret` in the examples with the ones you get from My Applications.
+Once you have created your app, replace the `client_id`, `redirect_uri` and `client_secret` in client.json with the ones you get from My Applications.
 
 ## Running the examples
 In order to run the different examples, open the folder with the name of the flow you want to try out, and run its `app.js` file. For instance, to run the Authorization Code example do:
--- authorization_code/app.js
+++ authorization_code/app.js
@@ -6,6 +6,9 @@
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
+// 
+
+var json = require('./client.json'); //with path
 
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
@@ -13,9 +16,10 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+
+var client_id = json.client_id ; // Your client id 
+var client_secret = json.client_secret ; // Your secret
+var redirect_uri = json.redirect_uri ; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -46,7 +50,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email user-read-recently-played user-read-currently-playing user-read-playback-state';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
